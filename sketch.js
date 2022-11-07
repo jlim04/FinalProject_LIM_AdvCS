0fd1dd73ceca3012999376f80259b52723d2d953
jlim04 <jae_lim@cate.org>
2022-11-06 20:26:00 -0800

    second change

--- sketch.js
+++ sketch.js
@@ -11,3 +11,7 @@ function setup() {
 
   //https://docs.google.com/document/d/1rm1MFeSm5X7rzaDxcykFrEfHo42_psByxt7qca1mVqo/edit
   //Right now, I'm using google docs to think of all the categories, sort of as a brainstorm. I have saved it with you.
+
+  //I have shifted almost all of the data on google docs into sheets so it is easier to utilize when coding.
+  //https://docs.google.com/spreadsheets/d/1bEYa_xUSEbtfwZd3KwkP4Wppt_zLWmmLdMczmKg7X2o/edit?usp=sharing
+  //jfw
