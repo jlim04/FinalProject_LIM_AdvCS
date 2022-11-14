137ecb5f6e661a47990116c979e311b6f5bb2d13
jlim04 <jae_lim@cate.org>
2022-11-13 21:53:34 -0800

    third

--- sketch.js
+++ sketch.js
@@ -1,5 +1,22 @@
+let table;
+
+function preload() {
+  //my table is comma separated value "csv"
+  //and has a header specifying the columns labels
+  table = loadTable('data.csv', 'csv', 'header');
+} 
+
 function setup() {
     createCanvas(400, 400);
+    print(table.getRowCount() + ' total rows in table');
+    print(table.getColumnCount() + ' total columns in table');
+    print(table.getColumn('name'));
+    // print(table.getString(0, 0));
+   
+    for (let r = 0; r < table.getRowCount(); r++)
+      for (let c = 0; c < table.getColumnCount(); c++) {
+        print(table.getString(r, c));
+      }
   }
   
   function draw() {
@@ -9,6 +26,8 @@ function setup() {
     }
   }
 
+ 
+
   //https://docs.google.com/document/d/1rm1MFeSm5X7rzaDxcykFrEfHo42_psByxt7qca1mVqo/edit
   //Right now, I'm using google docs to think of all the categories, sort of as a brainstorm. I have saved it with you.
 
@@ -17,3 +36,10 @@ function setup() {
   //this is my second day
 
   ///I shifted all the data onto the google sheets. I also outlined my specific plan and questions. I could address those with you tomorrow in class. 
+
+  //looked into the html concept as well as bootstrap and every single component in it. 
+  //found a few features I would like to use: Dropdown, I could conduct the survey through multiple dropdowns
+  //Navbar for extra information(don't think it is crucial though)
+  //placeholders for potential decoration
+  //toasts for the introduction
+  //will do further research on this tomorrow
