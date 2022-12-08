6c994ae0688085190f1b1849bf9483de944bc2b5
jlim04 <jae_lim@cate.org>
2022-12-08 09:28:03 -0800

    dec 8

--- sketch.js
+++ sketch.js
@@ -7,70 +7,70 @@ function preload() {
   table = loadTable('data.csv', 'csv', 'header');
 
   checkboxes[0] = createCheckbox('Small(5000>)', false);
-  checkboxes[0].parent("checkboxes");
+  checkboxes[0].parent("size");
   checkboxes[0].changed(()=> myCheckedEvent(0));
 
   checkboxes[1] = createCheckbox('Medium(5000-10000)', false);
-  checkboxes[1].parent("checkboxes");
+  checkboxes[1].parent("size");
   checkboxes[1].changed(() => myCheckedEvent(1));
   
   checkboxes[2] = createCheckbox('Large(10000<)', false);
-  checkboxes[2].parent("checkboxes");
+  checkboxes[2].parent("size");
   checkboxes[2].changed(() => myCheckedEvent(2));
 
   checkboxes[3] = createCheckbox('City', false);
-  checkboxes[3].parent("checkboxes");
+  checkboxes[3].parent("location");
   checkboxes[3].changed(() => myCheckedEvent(3));
 
 
   checkboxes[4] = createCheckbox('Suburban', false);
-  checkboxes[4].parent("checkboxes");
+  checkboxes[4].parent("location");
   checkboxes[4].changed(() => myCheckedEvent(4));
 
   checkboxes[5] = createCheckbox('Town', false);
-  checkboxes[5].parent("checkboxes");
+  checkboxes[5].parent("location");
   checkboxes[5].changed(() => myCheckedEvent(5));
 
   checkboxes[6] = createCheckbox('Rural', false);
-  checkboxes[6].parent("checkboxes");
+  checkboxes[6].parent("location");
   checkboxes[6].changed(() => myCheckedEvent(6));
 
   checkboxes[7] = createCheckbox('Has Greek Life', false);
-  checkboxes[7].parent("checkboxes");
+  checkboxes[7].parent("greeklife");
   checkboxes[7].changed(() => myCheckedEvent(7));
 
   checkboxes[8] = createCheckbox('Catholic', false);
-  checkboxes[8].parent("checkboxes");
+  checkboxes[8].parent("religion");
   checkboxes[8].changed(() => myCheckedEvent(8));
 
   checkboxes[9] = createCheckbox('Methodist', false);
-  checkboxes[9].parent("checkboxes");
+  checkboxes[9].parent("religion");
   checkboxes[9].changed(() => myCheckedEvent(9));
 
   checkboxes[10] = createCheckbox('Presbyterian', false);
-  checkboxes[10].parent("checkboxes");
+  checkboxes[10].parent("religion");
   checkboxes[10].changed(() => myCheckedEvent(10));
 
 
   checkboxes[11] = createCheckbox('Jewish', false);
-  checkboxes[11].parent("checkboxes");
+  checkboxes[11].parent("religion");
   checkboxes[11].changed(() => myCheckedEvent(11));
 
 
   checkboxes[12] = createCheckbox('Public', false);
-  checkboxes[12].parent("checkboxes");
+  checkboxes[12].parent("public");
   checkboxes[12].changed(() => myCheckedEvent(12));
 
   checkboxes[13] = createCheckbox('Has Student Government', false);
-  checkboxes[13].parent("checkboxes");
+  checkboxes[13].parent("etc");
   checkboxes[13].changed(() => myCheckedEvent(13));
 
   checkboxes[14] = createCheckbox('Has a Football Team', false);
-  checkboxes[14].parent("checkboxes");
+  checkboxes[14].parent("etc");
   checkboxes[14].changed(() => myCheckedEvent(14));
 
   checkboxes[15] = createCheckbox('Tech/Engineering School', false);
-  checkboxes[15].parent("checkboxes");
+  checkboxes[15].parent("etc");
   checkboxes[15].changed(() => myCheckedEvent(15));
 
 
